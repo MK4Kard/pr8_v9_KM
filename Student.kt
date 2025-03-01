ff4a87ce006cbe19f30bb10862175ff31cde6103
MK4Kard <misha212032@gmail.com>
2025-03-01 17:19:06 +0500

    Update Student.kt

--- Student.kt
+++ Student.kt
@@ -17,12 +17,37 @@ class Student(_sport: String) {
     fun Age():String{ //высчитывает сколько лет студенту
         var age = 2025 - year
         when {
-            (age < 15 || age > 26) -> return "Возраст не соответствует"
+            (age < 15 || age > 40) -> return "Возраст не соответствует"
         }
         return "Возраст: $age"
     }
     fun Sport():String{
-        return sport
+        // бокс, ММА, Дзюдо, борьба, лёгкая атлетика
+        var text = ""
+        when {
+            sport == "бокс" -> when {
+                (weight > 48 || weight < 91)&&(height > 160 || height < 172) -> text = "Подходит для $sport"
+                else -> text = "Не подходит"
+            }
+            sport == "ММА" -> when {
+                (weight > 56 || weight < 110)&&(height > 178 || height < 185) -> text = "Подходит для $sport"
+                else -> text = "Не подходит"
+            }
+            sport == "дзюдо" -> when {
+                (weight > 40 || weight < 100)&&(height > 174 || height < 185) -> text = "Подходит для $sport"
+                else -> text = "Не подходит"
+            }
+            sport == "борьба" -> when {
+                (weight > 56 || weight < 110)&&(height > 178 || height < 185) -> text = "Подходит для $sport"
+                else -> text = "Не подходит"
+            }
+            sport == "лёгкая атлетика" -> when {
+                (weight > 40 || weight < 75)&&(height > 160 || height < 200) -> text = "Подходит для $sport"
+                else -> text = "Не подходит"
+            }
+            else -> text = "Нет вида спорта"
+        }
+        return text
     }
     fun Static():String{ //выводит данные о студенте
         var age = 2025 - year
@@ -36,7 +61,7 @@ class Student(_sport: String) {
                 in 15..16 -> when{
                     (height < 156 || height > 180) -> return "Рост не соответствует нормам"
                 }
-                in 17..26 -> when{
+                in 17..40 -> when{
                     (height < 160 || height > 210) -> return "Рост не соответствует нормам"
                 }
             }
@@ -44,7 +69,7 @@ class Student(_sport: String) {
                 in 15..16 -> when{
                     (height < 154 || height > 170) -> return "Рост не соответствует нормам"
                 }
-                in 17..26 -> when{
+                in 17..40 -> when{
                     (height < 163 || height > 200) -> return "Рост не соответствует нормам"
                 }
             }
@@ -54,20 +79,20 @@ class Student(_sport: String) {
                 in 15..16 -> when{
                     (weight < 45 || weight > 76) -> return "Вес не соответствует нормам"
                 }
-                in 17..26 -> when{
-                    (weight < 55 || weight > 80) -> return "Вес не соответствует нормам"
+                in 17..40 -> when{
+                    (weight < 55 || weight > 110) -> return "Вес не соответствует нормам"
                 }
             }
             (gender == "Ж") -> when (age){
                 in 15..16 -> when{
-                    (weight < 154 || weight > 170) -> return "Вес не соответствует нормам"
+                    (weight < 44 || weight > 60) -> return "Вес не соответствует нормам"
                 }
-                in 17..26 -> when{
-                    (weight < 163 || weight > 200) -> return "Вес не соответствует нормам"
+                in 17..40 -> when{
+                    (weight < 50 || weight > 70) -> return "Вес не соответствует нормам"
                 }
             }
         }
-        return "Группа: $group" +
+        return "Группа: $group\n" +
                 "Пол: $gender, Рост: $height, Вес: $weight"
     }
 }
